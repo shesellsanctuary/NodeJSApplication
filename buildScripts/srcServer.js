b77853da4568215058b72e8ef4a55e5cae6a654f
Calvet <emily.calvet@sap.com>
2018-09-11 17:31:14 -0300

    Solve static files path on express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,6 +5,8 @@ var open = require('open');
 var port = 3000;
 var app = express();
 
+app.use(express.static(path.join(__dirname, '../src/')));
+
 app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
